2e65c9517a909b5654a2e39e92e0dca9b9c71d02
Reinaldo Yabiku <reinaldoybk@hotmail.com>
2023-04-23 04:28:28 -0300

    05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ( a < 0 || b < 0) return "ERROR";
+    if (typeof(a) != "number" || typeof(b) != "number") return "ERROR";
 
+    let argsSum = a + b;
+    let gauss = 0;
+
+    a > b ? gauss = a / 2 : gauss = b / 2;
+
+    let res = argsSum * gauss;
+
+    return res;
 };
 
 // Do not edit below this line
