1b63c835493d7e983e1734ed9ea64f71aa070af1
Reinaldo Yabiku <reinaldoybk@hotmail.com>
2023-04-23 07:04:46 -0300

    07 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const res = (temp - 32) / 1.8;
+  
+  return Math.round(res * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const res = (temp * 1.8) + 32;
+
+  return Math.round(res * 10) / 10;
 };
 
 // Do not edit below this line
