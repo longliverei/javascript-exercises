d239ee685230881ccd3225de46ffc0e3fe23be6a
Reinaldo Yabiku <reinaldoybk@hotmail.com>
2023-04-27 22:54:04 -0300

    09 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newStr = str.replace(/[^\w\s\']|_/g, "")
+                .replace(/\s+/g, "");
 
+    let res = newStr.toLowerCase();
+    let len = res.length - 1;
+
+    for (let i = 0; i < res.length; i++) {
+        if (res[i] != res[len--]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
