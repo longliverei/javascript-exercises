0ae4f742ce1b129682b5cceb1155a41d0185bf60
Reinaldo Yabiku <reinaldoybk@hotmail.com>
2023-04-28 00:08:33 -0300

    10 solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 1) return "OOPS";
+    let i = 2;
+    let n = parseInt(num);
+    let fib = new Array();
+    fib[0] = 0;
+    fib[1] = 1;
 
+    for (i; i <= n; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+
+    return fib[n];
 };
 
 // Do not edit below this line
